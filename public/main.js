17a44b8931f3e85fa57d0bcd8f780fcbdd46d4a4
bakitucker <bakitucker8@gmail.com>
2023-05-19 15:34:10 -0400

    update

--- public/main.js
+++ public/main.js
@@ -15,6 +15,11 @@
         .then(res => res.json()) // parse response as JSON 
     .then(data => { 
       console.log(data) 
+      document.querySelector('.muscle').innerText = data[0].muscle.toUpperCase()
+      document.querySelector('.workoutName').innerText = data[0].name
+      document.querySelector('.difficulty').innerText = data[0].difficulty.toUpperCase()
+
+      document.querySelector('.instructions').innerText = data[0].instructions
  
     }) 
     .catch(err => { 
